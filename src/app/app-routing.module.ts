05426a38df83c27816b0a146fd15aace0476c566
Natan Zunfrfilli <natanzun@outlook.com.br>
2023-08-15 21:10:21 -0300

    Page Parameter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { TestComponent } from './pages/test/test.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { AboutComponent } from './pages/about/about.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'contact-us', component: ContactUsComponent},
   { path: 'test', component: TestComponent },
   { path: 'not-found', component: NotFoundComponent },
+  { path: 'about', component: AboutComponent },
 
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/not-found', pathMatch: 'full' },
