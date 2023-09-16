74ad1f22dc8420192f23b865dee86832c09fb42a
dudushy <eduardotalarico1337@gmail.com>
2023-09-15 21:16:52 -0300

    Create projects page

---
+++ src/app/pages/projects/projects.component.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { AppComponent } from '../../app.component';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.scss']
+})
+export class ProjectsComponent implements OnInit {
+  title = 'ProjectsComponent';
+
+  constructor(
+    private cdr: ChangeDetectorRef,
+    public app: AppComponent
+  ) {
+    console.log(`[${this.title}#constructor]`);
+  }
+
+  ngOnInit(): void {
+    console.log(`[${this.title}#ngOnInit]`);
+  }
+
+  updateView() {
+    console.log(`[${this.title}#updateView]`);
+
+    this.cdr.detectChanges;
+    this.app.updateView(this.title);
+  }
+
+  async redirectTo(url: any) {
+    await this.app.redirectTo(url, this.title);
+
+    this.updateView();
+  }
+
+  defaultOrder() { return 0; }
+}
