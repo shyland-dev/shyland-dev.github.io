7fef5b80a2c09d3fc17f0173e2e0bf2ee62559b4
dudushy <eduardotalarico1337@gmail.com>
2023-10-11 23:30:13 -0300

    Creating card animation (WIP)

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -10,6 +10,51 @@ import { AppComponent } from '../../app.component';
 export class AboutComponent implements OnInit {
   title = 'AboutComponent';
 
+  expandedCardIndex = '-1';
+  cardArray = [
+    {
+      img: 'assets/imgs/PNGs/dudushy.png',
+      name: 'Eduardo "dudushy" Talarico',
+      mainRole: 'Founder',
+      roles: [
+        'Fundador',
+        'Co-idealizador'
+      ],
+      description: 'da ShylandDev, possui vasta experiência no ramo de criação de aplicativos, sites, dentre outras smart solutions para empresas pequeno e médio porte.',
+      social: [
+        {
+          network: 'GitHub',
+          url: 'https://github.com/dudushy'
+        }
+      ],
+      formation: [
+        'Graduação Superior em Ciências da Computação pela Universidade Paulista',
+        'Web Developer (Ionic and Angular)'
+      ]
+    },
+    {
+      img: 'assets/imgs/PNGs/natzun.png',
+      name: 'Natan "Natzun" Zunfrilli',
+      mainRole: 'Co-founder',
+      roles: [
+        'Co-fundador',
+        'Co-idealizador'
+      ],
+      description: 'da ShylandDev, possui 5 anos de experiência e atuação no ramo do direito imobiliário registral, com desenvolvimento smart solutions para cartórios de Registro de Imóveis. Atuando como Analista de implantação, suporte e desenvolvimento.',
+      social: [
+        {
+          network: 'GitHub',
+          url: 'https://github.com/Natzun'
+        }
+      ],
+      formation: [
+        'Graduação Superior em Direito pelo Centro Universitário Barão de Mauá',
+        'IT Security Specialist (database analysis and cyber attack)',
+        'C# Developer'
+      ]
+    }
+  ];
+
   constructor(
     private cdr: ChangeDetectorRef,
     public app: AppComponent
@@ -19,6 +64,9 @@ export class AboutComponent implements OnInit {
 
   ngOnInit(): void {
     console.log(`[${this.title}#ngOnInit]`);
+
+    console.log(`[${this.title}#ngOnInit] expandedCardIndex`, this.expandedCardIndex);
+    console.log(`[${this.title}#ngOnInit] cardArray`, this.cardArray);
   }
 
   updateView() {
@@ -35,4 +83,12 @@ export class AboutComponent implements OnInit {
   }
 
   defaultOrder() { return 0; }
+
+  expandCard(index) {
+    console.log(`[${this.title}#expandCard] index`, index);
+
+    this.expandedCardIndex = index;
+
+    console.log(`[${this.title}#expandCard] expandedCardIndex`, this.expandedCardIndex);
+  }
 }
